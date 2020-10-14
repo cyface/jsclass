c134bff975abbd6eac2970276a21501532678092
Tim White <tim@cyface.com>
2020-10-14 13:43:51 -0600

    Balanced brackets

--- brackets_balanced.js
+++ brackets_balanced.js
@@ -5,16 +5,17 @@ Requirements were not to make sure they were match in *nested pairs*, just that 
  */
 
 let bracketsBalanced = stringToSearch => {
-  const left = stringToSearch.match(/\[+/gm) || 0
-  const right = stringToSearch.match(/]+/gm) || 0
+  const left = stringToSearch.match(/\[/gm) || 0
+  const right = stringToSearch.match(/]/gm) || 0
   return left.length === right.length
 }
 
-console.log(bracketsBalanced('bob')) // should be true
-console.log(bracketsBalanced('[bob')) // false
-console.log(bracketsBalanced('[bob]')) // should be true
-console.log(bracketsBalanced('[[[bob')) // false
-console.log(bracketsBalanced('[[bob]]')) // should be true
+console.log(bracketsBalanced('bob')) // true
+console.log(bracketsBalanced('[bob]')) // true
+console.log(bracketsBalanced('[[bob]]')) //
+console.log(bracketsBalanced('[bob\n\n]')) // true
+console.log(bracketsBalanced('][bob\n\n][')) // true
+console.log(bracketsBalanced('i [am[]wr[o]ng[]]')) // true
 console.log(bracketsBalanced('[]bob[')) // false
 console.log(bracketsBalanced('[[[bob')) // false
 console.log(bracketsBalanced('bob]')) // false
