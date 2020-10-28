65de561ff4172373ab0528e546d749eb302b85b8
Tim White <tim@cyface.com>
2020-10-28 10:41:19 -0600

    Sum and Product till one digit challenge.

--- sum_and_product_till_one_digit.js
+++ sum_and_product_till_one_digit.js
@@ -12,6 +12,9 @@
  **/
 
 function sumAndProductTillSingle () {
+  /* args: any number of integers as separate args
+   * return: sum of args, with each resulting digit multiplied times each other until product is one digit
+   */
   const args = Array.from(arguments) // Convert arguments to an array we can reduce
 
   const argSum = args.reduce((runningSum, currentNum) => runningSum + currentNum) // Sum all args
@@ -32,3 +35,11 @@ function sumAndProductTillSingle () {
 console.log(`Final Answer: ${sumAndProductTillSingle(16, 28)}`) // Should be 6
 console.log(`Final Answer: ${sumAndProductTillSingle(0)}`) // Should be 0
 console.log(`Final Answer: ${sumAndProductTillSingle(1, 2, 3, 4, 5, 6)}`) // Should be 2
+
+
+/*
+* Notes :
+* I wouldn't make a function that took unbounded args.  If I really needed to do this, I'd make it take an array of Ints
+* Recursion is probably the right way to handle this, but might be possible to do it with nested reduce.
+* This doesn't handle floats/doubles or non-number args correctly
+ */
